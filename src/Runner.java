f6c6fb0e4d2034482df6d2b75c3bb0ddaf446a7e
Abdul Rafi <arafi6624@bths.edu>
2018-11-27 22:44:08 -0500

    Completed

--- src/Runner.java
+++ src/Runner.java
@@ -1,24 +1,45 @@
 public class Runner {
     public static void main(String[] args){
-        String[][] face = new String[5][8];
+        String[][] face = new String[11][11];
         JackOLantern face1 = new JackOLantern(face);
 
-        face1.fill("-");
+        face1.fill("x");
         face1.edit(" ",0,0);
-        face1.edit(" ",4,7);
-        face1.edit(" ",0,7);
-        face1.edit(" ",4,0);
+        face1.edit(" ",0,10);
+        face1.edit(" ",10,0);
+        face1.edit(" ",10,10);
+        face1.edit(" ",2,2);
+        face1.edit(" ",2,8);
+        face1.edit(" ",5,5);
+        face1.edit(" ",7,7);
 
-        face1.edit("0",1,2);
-        face1.edit("0",1,5);
-        face1.edit("^",2,3);
-        face1.edit("^",2,4);
-        face1.edit("|",3,1);
-        face1.edit("|",3,2);
-        face1.edit("_",3,4);
-        face1.edit("_ ",3,4);
-        face1.edit("|",3,5);
-        face1.edit("|",3,6);
+        face1.edit("-",1,1);
+        face1.edit("-",1,2);
+        face1.edit("-",1,3);
+        face1.edit("|",2,1);
+        face1.edit("|",2,3);
+        face1.edit("-",3,1);
+        face1.edit("-",3,2);
+        face1.edit("-",3,3);
+
+        face1.edit("-",1,7);
+        face1.edit("-",1,8);
+        face1.edit("-",1,9);
+        face1.edit("|",2,7);
+        face1.edit("|",2,9);
+        face1.edit("-",3,7);
+        face1.edit("-",3,8);
+        face1.edit("-",3,9);
+
+        face1.edit("-",5,4);
+        face1.edit("-",5,5);
+        face1.edit("-",5,6);
+        face1.edit("|",6,4);
+        face1.edit("|",6,6);
+        face1.edit("-",7,4);
+        face1.edit("-",7,5);
+        face1.edit("-",7,6);
+
         System.out.println(face1);
     }
 }
